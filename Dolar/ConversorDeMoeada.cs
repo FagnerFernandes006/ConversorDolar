2298c89edc5fae3a5fb76ba3d22cb50c40b6f559
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2022-05-27 16:22:53 -0300

    Conversor de moeadas(Dólar) com valor IOF

---
+++ Dolar/ConversorDeMoeada.cs
@@ -0,0 +1,12 @@
+﻿namespace Dolar
+{
+    public class ConversorDeMoeada
+    {
+        public static double IOF = 0.06;
+
+        public static double Valor(double dolar, int qtde)
+        {
+            return dolar * qtde + (dolar * IOF * qtde);
+        }
+    }
+}
---
+++ Dolar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Dolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dólar Hoje?: ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você irá comprar? ");
+            int qtde = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valor = ConversorDeMoeada.Valor(dolar, qtde);
+            Console.WriteLine("Valor a ser pago: R$" + valor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
